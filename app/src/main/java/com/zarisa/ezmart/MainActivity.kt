03bacf7f25f5c763a7f4c56010f4de99fa4cb0ef
Zarisa <jannesari.zarisa@gmail.com>
2022-06-06 00:34:24 +0430

    primary setup for splash screen with test icon. waiting for EZ to get ready

--- app/src/main/java/com/zarisa/ezmart/MainActivity.kt
+++ app/src/main/java/com/zarisa/ezmart/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zarisa.ezmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,16 +11,32 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.zarisa.ezmart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setSupportActionBar(binding.viewMainContent.toolbar)
+        setUpSplashScreen()
         setUpBottomNavigation()
     }
 
+    private fun setUpSplashScreen() {
+        binding.iconSplashScreen.let {
+            it.alpha = 0f
+            it.animate().setDuration(1500).alpha(1f).withEndAction {
+                binding.viewMainContent.root.visibility = View.VISIBLE
+                binding.iconSplashScreen.visibility = View.GONE
+                supportActionBar?.show()
+                overridePendingTransition(
+                    android.R.anim.fade_in,
+                    android.R.anim.fade_out
+                )
+            }
+        }
+    }
+
     private fun setUpBottomNavigation() {
+        setSupportActionBar(binding.viewMainContent.toolbar)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
         val navController = navHostFragment.navController
