4557188f926e6f26372b87a3d5f31edd4d0189aa
Zarisa <jannesari.zarisa@gmail.com>
2022-06-05 21:36:57 +0430

    bind bottom navigation in main activity code

--- app/src/main/java/com/zarisa/ezmart/MainActivity.kt
+++ app/src/main/java/com/zarisa/ezmart/MainActivity.kt
@@ -2,10 +2,28 @@ package com.zarisa.ezmart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.zarisa.ezmart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.viewMainContent.toolbar)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
+        val navController = navHostFragment.navController
+        findViewById<BottomNavigationView>(R.id.bottom_nav)
+            .setupWithNavController(navController)
     }
 }
