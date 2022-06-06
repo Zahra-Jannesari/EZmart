058d0acf6d1ad310428eb62cfbfd12f3aaf43f8c
Zarisa <jannesari.zarisa@gmail.com>
2022-06-06 16:41:07 +0430

    move existing fragments and activity into ui package

--- app/src/main/java/com/zarisa/ezmart/ui/MainActivity.kt
+++ app/src/main/java/com/zarisa/ezmart/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zarisa.ezmart
+package com.zarisa.ezmart.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.zarisa.ezmart.R
 import com.zarisa.ezmart.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/zarisa/ezmart/ui/category/CategoriesFragment.kt
+++ app/src/main/java/com/zarisa/ezmart/ui/category/CategoriesFragment.kt
@@ -1,10 +1,11 @@
-package com.zarisa.ezmart
+package com.zarisa.ezmart.ui.category
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.zarisa.ezmart.R
 
 
 class CategoriesFragment : Fragment() {
