d0eb751986efdeef38638c362e0facabc1ea71b7
Zarisa <jannesari.zarisa@gmail.com>
2022-06-06 17:20:59 +0430

    add ProductRemoteDataSource

---
+++ app/src/main/java/com/zarisa/ezmart/data/product/ProductRemoteDataSource.kt
@@ -0,0 +1,7 @@
+package com.zarisa.ezmart.data.product
+
+import com.zarisa.ezmart.data.network.ApiService
+import javax.inject.Inject
+
+class ProductRemoteDataSource@Inject constructor(private val apiService: ApiService) {
+}
