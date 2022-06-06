b1918e2b5ae5b0bc7d00bd1df0f64f34f237caa9
Zarisa <jannesari.zarisa@gmail.com>
2022-06-06 18:30:20 +0430

    add basic params in networkParams class

---
+++ app/src/main/java/com/zarisa/ezmart/data/network/NetworkParams.kt
@@ -0,0 +1,11 @@
+package com.zarisa.ezmart.data.network
+
+class NetworkParams {
+    companion object {
+
+        const val BASE_URL = "https://woocommerce.maktabsharif.ir/wp-json/wc/v3"
+        const val CONSUMER_KEY= "ck_cfa862db1553b15dc59f6b27b14f343fe5f74e0e"
+        const val CONSUMER_SECRET="cs_5de453d21f1ff46c90f7412f9e441882574d9af2"
+
+    }
+}
