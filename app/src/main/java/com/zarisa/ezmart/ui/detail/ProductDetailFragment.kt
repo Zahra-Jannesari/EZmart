1c8a1928f25498ccd266fbeeaf2c5f42020c3bc9
Zarisa <jannesari.zarisa@gmail.com>
2022-06-08 00:34:03 +0430

    call getProductById in ProductDetailFragment by the id sent from previous page and bind the view model to view

--- app/src/main/java/com/zarisa/ezmart/ui/detail/ProductDetailFragment.kt
+++ app/src/main/java/com/zarisa/ezmart/ui/detail/ProductDetailFragment.kt
@@ -5,17 +5,36 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.zarisa.ezmart.R
+import androidx.fragment.app.viewModels
+import com.zarisa.ezmart.databinding.FragmentProductDetailBinding
+import com.zarisa.ezmart.ui.home.PRODUCT_ID
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class ProductDetailFragment : Fragment() {
-
+    private lateinit var binding: FragmentProductDetailBinding
+    private val viewModel: ProductDetailViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    ): View {
+        binding = FragmentProductDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        getCurrentProduct()
+        bindView()
+    }
+
+    private fun bindView() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+    }
+
+    private fun getCurrentProduct() {
+        viewModel.getProductById(requireArguments().getInt(PRODUCT_ID))
     }
 
 }
