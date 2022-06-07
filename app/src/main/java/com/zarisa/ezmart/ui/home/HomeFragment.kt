2cc741f85ff7435c41c8d9d34119c32aaf90f6cd
Zarisa <jannesari.zarisa@gmail.com>
2022-06-07 13:46:06 +0430

    bind recycler views and use listData to submit lists

--- app/src/main/java/com/zarisa/ezmart/ui/home/HomeFragment.kt
+++ app/src/main/java/com/zarisa/ezmart/ui/home/HomeFragment.kt
@@ -5,18 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.zarisa.ezmart.R
+import com.zarisa.ezmart.databinding.FragmentHomeBinding
+import com.zarisa.ezmart.ui.components.RecyclerViewAdapter
 
 
 class HomeFragment : Fragment() {
 
-
+    private lateinit var binding: FragmentHomeBinding
+    private val viewModel: HomeViewModel by viewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindView()
+    }
+
+    private fun bindView() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.rvNewest.adapter = RecyclerViewAdapter()
+        binding.rvMostSeen.adapter = RecyclerViewAdapter()
+        binding.rvHighRates.adapter = RecyclerViewAdapter()
     }
 
 
