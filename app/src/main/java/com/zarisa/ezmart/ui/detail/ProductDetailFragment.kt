55e3f78cbeaa9d69b923fb20077ba61d10ccd7c4
Zarisa <jannesari.zarisa@gmail.com>
2022-06-07 23:14:57 +0430

    add fragment for detail with an action from home fragment

---
+++ app/src/main/java/com/zarisa/ezmart/ui/detail/ProductDetailFragment.kt
@@ -0,0 +1,21 @@
+package com.zarisa.ezmart.ui.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zarisa.ezmart.R
+
+
+class ProductDetailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_product_detail, container, false)
+    }
+
+}
