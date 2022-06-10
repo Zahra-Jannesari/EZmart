2ddac79d5a04d4c167eb203254df1c3022669f2f
Zarisa <jannesari.zarisa@gmail.com>
2022-06-10 12:02:20 +0430

    FEATURE: CONNECTION CHECK IN SPLASH SCREEN

--- app/src/main/java/com/zarisa/ezmart/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/zarisa/ezmart/ui/splash/SplashActivity.kt
@@ -1,13 +1,18 @@
 package com.zarisa.ezmart.ui.splash
 
+import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import com.zarisa.ezmart.R
 import com.zarisa.ezmart.databinding.ActivitySplashScreenBinding
 import com.zarisa.ezmart.ui.MainActivity
+import java.net.UnknownHostException
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     lateinit var binding: ActivitySplashScreenBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,12 +27,47 @@ class SplashActivity : AppCompatActivity() {
         window.statusBarColor = ContextCompat.getColor(this, R.color.golden)
         binding.iconSplashScreen.let {
             it.alpha = 0f
-            it.animate().setDuration(1500).alpha(1f).withEndAction {
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent)
-                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
-                finish()
+            it.animate().setDuration(1500).alpha(1f).let { animation ->
+                if (isInternetAvailable())
+                    animation.withEndAction {
+                        intentToMainActivity()
+                    }
+                else {
+                    binding.refreshConnection.let { layout ->
+                        layout.visibility = View.VISIBLE
+                        layout.setOnClickListener { view ->
+                            onRefreshClick(view)
+                        }
+                    }
+                }
             }
         }
+    }
+
+    private fun intentToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
+    }
+
+    private fun onRefreshClick(view: View) {
+        view.visibility = View.INVISIBLE
+        if (isInternetAvailable())
+            intentToMainActivity()
+        else
+            view.visibility = View.VISIBLE
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        try {
+            val command = "ping -c 1 google.com"
+            return Runtime.getRuntime().exec(command).waitFor() == 0
+        } catch (e: UnknownHostException) {
+            Log.d("TAG", "isInternetAvailable: false")
+        } catch (e: Exception) {
+            Log.d("TAG", "unknown exception")
+        }
+        return false
     }
 }
