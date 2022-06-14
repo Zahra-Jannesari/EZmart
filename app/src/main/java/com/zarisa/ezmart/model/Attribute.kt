950ce57f44561afd84917ecc6d92133306d9056f
Zarisa <jannesari.zarisa@gmail.com>
2022-06-14 22:32:57 +0430

    improve: additional fields in models for search and filter

---
+++ app/src/main/java/com/zarisa/ezmart/model/Attribute.kt
@@ -0,0 +1,7 @@
+package com.zarisa.ezmart.model
+
+data class Attribute(
+    val id:Int,
+val name   : String   ,
+val options :List<AttributeTerms>
+)
---
+++ app/src/main/java/com/zarisa/ezmart/model/AttributeTerms.kt
@@ -0,0 +1,7 @@
+package com.zarisa.ezmart.model
+
+data class AttributeTerms(
+    val id: Int,
+    val name: String?,
+    val description: String
+)
